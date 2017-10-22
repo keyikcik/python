6ffc2fb9bace3e40a43475fb6927a5c58af702b2
Ayşegül Tan <30315736+keyikcik@users.noreply.github.com>
2017-10-22 13:56:36 +0300

    Add files via upload

---
+++ StartPython.py
@@ -0,0 +1,34 @@
+# -StartPython.py *- coding: utf-8 -*-
+"""
+Spyder Editor
+
+#%% starts a new cell. Use second green triangle to run just the cell that
+your mouse has last clicked in (or Ctrl-Enter on a PC or Command-Return on a
+Macintosh or Menu>Run>Run Cell)
+"""
+#%%
+def hello():
+    print("Hello, world!")
+
+#%%
+def myname():
+    print("My name is Aysegul")
+    
+#%%
+def ourschool():
+    print("I begin learning PYHTON :) ")
+#%%
+def deneme():
+    print("Do you love me? -yes I do Narsis :) ")
+#%%  
+  
+""" This will run forever. In this case Ctrl-C will stop it, but sometimes
+that doesn't work. In that case, click away IP Console to stop; click yes to 
+kill kernel. Open a new IPConsole on the Console menu to restart """
+#%%
+def forever():
+    while True:
+        pass
+  
+#%% 
+    
